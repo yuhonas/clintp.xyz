a87e931cfe20a95d267df51a10cf7cf0000e7ae9
Clint Plummer <clint@yuhonas.com>
2020-04-13 14:17:23 -0700

    few tweaks + add eslint in prep for linting

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,10 @@
 import Head from 'next/head'
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-};
+// const layoutStyle = {
+//   margin: 20,
+//   padding: 20,
+//   border: '1px solid #DDD'
+// };
 
 const Layout = props => (
 <div class="container">
