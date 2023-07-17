1253d104b1ce4aca8e6baec3fc9df7ef33f0d7e0
Clint Plummer <clint@yuhonas.com>
2023-07-17 14:06:12 -0400

    working on auto scroll

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { useData } from 'vitepress'
-const { page } = useData()
+const { page } = useData();
 </script>
 
 <template>
   <h1 class="font-extrabold text-white pt-1 ms-4 text-lg">$ ./clintp.xyz</h1>
   <img src="./images/background.jpg" class="w-full object-cover absolute top-0 left-0 h-full -z-50" />
 
-  <div class="bg-slate-900 rounded-t-md p-3 mx-auto mt-4 font-mono max-w-2xl ">
+  <div class="bg-slate-900 rounded-t-md p-3 mx-auto mt-4 max-w-2xl ">
       <div class="flex gap-1.5">
         <div class="w-2.5 h-2.5 bg-red-400 rounded-full"></div>
         <div class="w-2.5 h-2.5 bg-yellow-400 rounded-full"></div>
