ab903ced37ba6ed8c09580b89e1aca1689f04b7c
Clint Plummer <clint@yuhonas.com>
2023-07-17 19:08:34 -0400

    add github corner

---
+++ docs/.vitepress/theme/GithubCorner.vue
@@ -0,0 +1,18 @@
+<template>
+  <p> testing</p>
+  <a href="https://github.com/yuhonas/clintp.xyz/">
+    <img alt="github" src="./images/github-logo.svg" class="z-50">
+  </a>
+</template>
+
+<style scoped>
+/* img {
+  fill: #64CEAA;
+  color: red;
+  position: absolute;
+  top: 0px;
+  border: 0px;
+  right: 0px;
+  width: auto;
+} */
+</style>
