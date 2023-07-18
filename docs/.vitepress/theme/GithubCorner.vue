f143237ee3d21b0e52ff9ffb549ec513f99697e8
Clint Plummer <clint@yuhonas.com>
2023-07-17 21:19:22 -0400

    update github corner logo for both breakpoints

--- docs/.vitepress/theme/GithubCorner.vue
+++ docs/.vitepress/theme/GithubCorner.vue
@@ -1,6 +1,7 @@
 <template>
   <a href="https://github.com/yuhonas/clintp.xyz/">
-    <img alt="github" src="./images/github-logo.svg" class="z-10 absolute top-0 right-0 w-12 md:w-auto">
+    <!-- <img alt="github" src="./images/github-corner.svg" class="z-10 absolute top-0 right-0 w-12 md:w-auto"> -->
+    <img alt="github" src="./images/github-logo.svg" class="z-10 absolute top-2 right-3 w-5 md:w-8">
   </a>
 </template>
 
