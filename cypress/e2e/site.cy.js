62d988ebced768cbce6aae1b0fb933ec2e011e03
Clint Plummer <clint@yuhonas.com>
2024-02-11 18:03:10 +1100

    add catchall for broken links, nothing worse then a 404 you don't know about

--- cypress/e2e/site.cy.js
+++ cypress/e2e/site.cy.js
@@ -5,6 +5,41 @@ context("site", () => {
     cy.visit("/");
   });
 
+  context("navigation", () => {
+    // this is just a catch all for other links on the page that i dont want to test individually
+    it("should have all working links", () => {
+      cy.get("a[href^='http']").each(($anchor) => {
+        cy.request($anchor.attr("href")).its("status").should("eq", 200);
+      });
+    });
+
+    it("should have a working link to my resume", () => {
+      cy.get("a[href='/resume.clintp.docx']")
+        .should("exist")
+        .then(($anchor) => {
+          cy.request($anchor.attr("href")).its("status").should("eq", 200);
+        });
+    });
+
+    it("should have a working link to my python notebook", () => {
+      cy.get(
+        "a[href='https://github.com/yuhonas/clintp.xyz/blob/main/resume/resume.clintp.ipynb']"
+      )
+        .should("exist")
+        .then(($anchor) => {
+          cy.request($anchor.attr("href")).its("status").should("eq", 200);
+        });
+    });
+
+    it("should have a working link to my linkedin", () => {
+      cy.get("a[href='https://www.linkedin.com/in/clint-plummer/']")
+        .should("exist")
+        .then(($anchor) => {
+          cy.request($anchor.attr("href")).its("status").should("eq", 200);
+        });
+    });
+  });
+
   it("should have the correct meta title", () => {
     cy.title().should("eq", "clintp.xyz | my bio in two mouse clicks or less");
   });
@@ -21,31 +56,5 @@ context("site", () => {
 
   it("should have my email address somewhere so I can be contacted", () => {
     cy.contains("hello@clintp.xyz");
-  });
-
-  it("should have a working link to my resume", () => {
-    cy.get("a[href='/resume.clintp.docx']")
-      .should("exist")
-      .then(($anchor) => {
-        cy.request($anchor.attr("href")).its("status").should("eq", 200);
-      });
-  });
-
-  it("should have a working link to my python notebook", () => {
-    cy.get(
-      "a[href='https://github.com/yuhonas/clintp.xyz/blob/main/resume/resume.clintp.ipynb']"
-    )
-      .should("exist")
-      .then(($anchor) => {
-        cy.request($anchor.attr("href")).its("status").should("eq", 200);
-      });
-  });
-
-  it("should have a working link to my linkedin", () => {
-    cy.get("a[href='https://www.linkedin.com/in/clint-plummer/']")
-      .should("exist")
-      .then(($anchor) => {
-        cy.request($anchor.attr("href")).its("status").should("eq", 200);
-      });
   });
 });
